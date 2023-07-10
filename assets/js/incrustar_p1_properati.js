1b614f1ce5e1d4dd98b1c5b349fae7aa8f12c2b5
Chelsy Mena | Andercol <124075503+chelsy-mena@users.noreply.github.com>
2023-07-10 17:36:17 -0500

    incrustando el html del notebook de properati

---
+++ assets/js/incrustar_p1_properati.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function() {
+    // Seleccionar el contenedor donde se cargará el Pedazo HTML
+    var contenedor = document.getElementById('notebook_properati');
+
+    // Crear un objeto XMLHttpRequest para cargar el Pedazo de  HTML
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                // Cargar el contenido HTML en el contenedor
+                contenedor.innerHTML = xhr.responseText;
+            } else {
+                console.error('Error al cargar el archivo.');
+            }
+        }
+    };
+
+    // Obtener el  pedazo de  HTML
+    xhr.open('GET', '../originales/notebook_p1_properati.html', true);
+    xhr.send();
+});
